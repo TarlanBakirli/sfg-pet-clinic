53001912a240becd655a9a5b4e883ff5e3a64d30
TarlanBakirli <terlanbekirli@gmail.com>
2019-07-16 01:14:01 +0400

    Implement services for interfaces. Closes #5

---
+++ pet-clinic-data/src/main/java/com/tarlanbakirli/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,17 @@
+package com.tarlanbakirli.sfgpetclinic.services;
+
+import com.tarlanbakirli.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByLastName(String lastName);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+
+}

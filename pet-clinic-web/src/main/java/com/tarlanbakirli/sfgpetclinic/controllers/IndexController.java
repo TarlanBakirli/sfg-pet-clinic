17b8be7e2f276d260a4b2a21f738da1dbe7baf2b
TarlanBakirli <terlanbekirli@gmail.com>
2019-07-16 17:22:00 +0400

    Adding Index Page and Controller. Closes #8

---
+++ pet-clinic-web/src/main/java/com/tarlanbakirli/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,19 @@
+package com.tarlanbakirli.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Tarlan Bakirli on 7/16/2019
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+
+        return "index";
+    }
+
+}

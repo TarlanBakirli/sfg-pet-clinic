4558f353b8134e1b86c017c3d7fe76068aef4172
TarlanBakirli <terlanbekirli@gmail.com>
2019-07-24 19:39:39 +0400

    added Pet SD JPA Service closes #43

--- pet-clinic-data/src/main/java/com/tarlanbakirli/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
+++ pet-clinic-data/src/main/java/com/tarlanbakirli/sfgpetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -3,10 +3,15 @@ package com.tarlanbakirli.sfgpetclinic.services.springdatajpa;
 import com.tarlanbakirli.sfgpetclinic.model.PetType;
 import com.tarlanbakirli.sfgpetclinic.repositories.PetTypeRepository;
 import com.tarlanbakirli.sfgpetclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+
+@Service
+@Profile("springdatajpa")
 public class PetTypeSDJpaService implements PetTypeService {
 
     private final PetTypeRepository petTypeRepository;

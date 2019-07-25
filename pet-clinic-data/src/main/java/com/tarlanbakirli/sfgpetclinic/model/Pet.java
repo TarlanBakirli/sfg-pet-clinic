3f5208174555897d427b2aa978db6e41b50424f9
TarlanBakirli <terlanbekirli@gmail.com>
2019-07-25 12:40:20 +0400

    refactored to leverage project Lombok. Closes #51

--- pet-clinic-data/src/main/java/com/tarlanbakirli/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/tarlanbakirli/sfgpetclinic/model/Pet.java
@@ -1,10 +1,17 @@
 package com.tarlanbakirli.sfgpetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "pets")
 public class Pet extends BaseEntity {
@@ -12,10 +19,12 @@ public class Pet extends BaseEntity {
     @Column(name = "name")
     private String name;
 
-    @Column(name = "type_id")
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
 
-    @Column(name = "owner_id")
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
 
     @Column(name = "birth_date")
@@ -24,43 +33,4 @@ public class Pet extends BaseEntity {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public PetType getPetType() {
-        return petType;
-    }
-
-    public void setPetType(PetType petType) {
-        this.petType = petType;
-    }
-
-    public Owner getOwner() {
-        return owner;
-    }
-
-    public void setOwner(Owner owner) {
-        this.owner = owner;
-    }
-
-    public LocalDate getBirthDate() {
-        return birthDate;
-    }
-
-    public void setBirthDate(LocalDate birthDate) {
-        this.birthDate = birthDate;
-    }
-
-    public Set<Visit> getVisits() {
-        return visits;
-    }
-
-    public void setVisits(Set<Visit> visits) {
-        this.visits = visits;
-    }
 }
--- pet-clinic-web/src/main/java/com/tarlanbakirli/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/tarlanbakirli/sfgpetclinic/bootstrap/DataLoader.java
@@ -61,7 +61,6 @@ public class DataLoader implements CommandLineRunner {
         Speciality savedDentistry = specialtyService.save(dentistry);
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
         owner1.setAddress("123 Brickerel");
@@ -77,10 +76,7 @@ public class DataLoader implements CommandLineRunner {
 
         ownerService.save(owner1);
 
-        ownerService.save(owner1);
-
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
         owner2.setAddress("123 Brickerel");
@@ -106,7 +102,6 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
         vet1.getSpecialities().add(savedRadiology);
@@ -114,7 +109,6 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
         vet2.getSpecialities().add(savedSurgery);
